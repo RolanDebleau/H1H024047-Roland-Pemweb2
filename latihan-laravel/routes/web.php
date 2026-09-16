1c3cbb61fa0cf6c497ea885b816c76c095b2d1af
RolanDebleau <gerardroland13@gmail.com>
2026-09-17 05:23:06 +0700

    Modul 3: migration, model, factory, seeder, dan CRUD dasar mahasiswa

--- latihan-laravel/routes/web.php
+++ latihan-laravel/routes/web.php
@@ -3,12 +3,14 @@
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\MahasiswaController;
 use App\Http\Controllers\MatakuliahController;
+use App\Http\Controllers\MahasiswaWebController;
 
 Route::get('/data-matakuliah', [MatakuliahController::class, 'index'])->name('matakuliah.index');
 Route::get('/data-matakuliah/{kode}', [MatakuliahController::class, 'show'])->name('matakuliah.show');
 Route::get('/data-mahasiswa', [MahasiswaController::class, 'index'])->name('mahasiswa.index');
 Route::get('/data-mahasiswa/{nim}', [MahasiswaController::class, 'show'])->name('mahasiswa.show');
 Route::get('/cari-mahasiswa', [MahasiswaController::class, 'cari'])->name('mahasiswa.cari');
+Route::get('/mahasiswa-data', [MahasiswaWebController::class, 'index'])->name('mahasiswa.data');
 
 Route::get('/salam', function () {
     return 'Selamat Datang di sini';
